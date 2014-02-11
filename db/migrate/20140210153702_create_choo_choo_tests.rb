1ef21cdc913071e6c155f2580c0bc3a517f65f1a
Alexander Hall <ah@alxhall.se>
2014-02-11 01:01:22 +0100

    Added default view template

---
+++ db/migrate/20140210153702_create_choo_choo_tests.rb
@@ -0,0 +1,8 @@
+class CreateChooChooTests < ActiveRecord::Migration
+  def change
+    create_table :choo_choo_tests do |t|
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/choo_choo_activities.rb
@@ -0,0 +1,6 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :choo_choo_activity, :class => 'Activity' do
+  end
+end
