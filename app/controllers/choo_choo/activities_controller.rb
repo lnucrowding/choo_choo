f697dad4b5239350928d512573e18e75296f4273
alcesleo <lagginglion@gmail.com>
2014-02-19 15:56:41 +0100

    More dynamic hello world with timestamps

--- app/controllers/choo_choo/activities_controller.rb
+++ app/controllers/choo_choo/activities_controller.rb
@@ -3,7 +3,6 @@ require_dependency "choo_choo/application_controller"
 module ChooChoo
   class ActivitiesController < ApplicationController
     def index
-
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,3 @@
 ChooChoo::Engine.routes.draw do
+  get '/activities/', to: 'activities#index'
 end
