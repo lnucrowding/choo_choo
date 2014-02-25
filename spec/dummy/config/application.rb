2796e7e5392f0ef5cfea6a181955c6b28f1c2a73
Alexander Hall <ah@alxhall.se>
2014-02-25 16:13:02 +0100

    Removed observer initialization

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -61,7 +61,6 @@ module Dummy
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.active_record.observers = :locomotive
   end
 end
 
