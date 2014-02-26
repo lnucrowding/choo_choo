5872fc03eaee48e406ed6dbde508bdd107a6d301
crowdingdev <linus@crowding.se>
2014-02-26 09:16:47 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 ChooChoo::Engine.routes.draw do
+  
+  # a intresting thing here would be to have altanative routes.
+  # one example is the facebook user profile where you can se another persons activities. 
+  # Then we cannot rely on current_user to know what should be shown. 
+  # Instead we need to send a id param to get another users activities.
+  # For example: get "activities/users/:id/", to: "activities#index"
+  
   get '/activities/', to: 'activities#index', as: 'activities'
 end
