d11bfa608f1b7d07ec6f4dc757a5c3d9ba05fe3f
alcesleo <lagginglion@gmail.com>
2014-03-03 13:28:03 +0100

    Created Post model

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140225222624) do
+ActiveRecord::Schema.define(:version => 20140303122720) do
 
   create_table "blogs", :force => true do |t|
     t.string   "title"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(:version => 20140225222624) do
   end
 
   create_table "choo_choo_locomotives", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
