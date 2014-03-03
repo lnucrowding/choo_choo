71b4821190a73841782940261a38ad825969c87a
alcesleo <lagginglion@gmail.com>
2014-03-03 18:05:30 +0100

    Posts belong to User

--- spec/dummy/app/models/post.rb
+++ spec/dummy/app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :title
 
+  belongs_to :user
+
   include ChooChoo::Concerns::Trackable::Parent
 end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140303122720) do
+ActiveRecord::Schema.define(:version => 20140303170331) do
 
   create_table "blogs", :force => true do |t|
     t.string   "title"
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(:version => 20140303122720) do
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer  "user_id"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
