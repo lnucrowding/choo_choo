cf9821098e98484e096b3ad7b1c3625169c87825
alcesleo <lagginglion@gmail.com>
2014-03-20 10:37:29 +0100

    Tested C(R)UD operations for parents and children

--- spec/models/choo_choo/activity_spec.rb
+++ spec/models/choo_choo/activity_spec.rb
@@ -2,6 +2,61 @@ require 'spec_helper'
 
 module ChooChoo
   describe Activity do
-    pending "add some examples to (or delete) #{__FILE__}"
+
+    describe "parent events" do
+      it "is created when a parent is created" do
+        expect {
+          create(:post)
+        }.to change {Activity.count}.by(1)
+      end
+
+      it "is updated when a parent is updated" do
+        post = create(:post)
+        comment = create(:comment, post: post)
+        post.update_attributes(title: 'Changed title')
+
+        expect(post.activity.last_action).to eq 'updated'
+        expect(post.activity.last_updated_node).to eq post
+      end
+
+      it "is left when a parent is deleted" do
+        post = create(:post)
+        activity = post.activity
+        post.destroy
+
+        expect(activity.last_action).to eq 'destroyed'
+        expect(activity.last_updated_node).to be_nil
+      end
+    end
+
+    describe "child events" do
+      it "is updated when a child is created" do
+        post = create(:post)
+        comment = create(:comment, post: post)
+
+        expect(post.activity.last_action).to eq 'created'
+        expect(post.activity.last_updated_node).to eq comment
+      end
+
+
+      it "is updated when a child is updated" do
+        post = create(:post)
+        comment = create(:comment, post: post)
+        comment.update_attributes(body: 'Changed comment')
+
+        expect(post.activity.last_action).to eq 'updated'
+        expect(post.activity.last_updated_node).to eq comment
+      end
+
+      it "is updated when a child is deleted" do
+        post = create(:post)
+        comment = create(:comment, post: post)
+        comment.destroy
+
+        expect(post.activity.last_action).to eq 'deleted'
+        expect(post.activity.last_updated_node).to eq comment
+      end
+
+    end
   end
 end
